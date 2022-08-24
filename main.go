8db18dfd456b26a7bf5cab2220e6a3442d515a05
Lucas Macedo <lucasmacedo9@hotmail.com>
2022-08-24 16:39:27 -0300

    atualizando

--- main.go
+++ main.go
@@ -2,11 +2,10 @@ package main
 
 import "github.com/gofiber/fiber/v2"
 
-
 func main() {
 	app := fiber.New()
 
-	app.Get("/", func(c *fiber.Ctx) error {
+	app.Get("/new", func(c *fiber.Ctx) error {
 		return c.SendString("Hello, World!")
 	})
 
